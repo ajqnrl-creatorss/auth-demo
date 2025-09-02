80fad4ef6d42cec16403f5876777172cf6ec8c9b
지헌 <ajqnrl789@naver.com>
2025-09-02 16:38:43 +0900

    카카오톡 로그인 API연동

---
+++ js/firebase/kakaoinit.js
@@ -0,0 +1,10 @@
+Kakao.init('4e32b4ac716986479266006a558d3f1c');
+Kakao.isInitialized();
+
+console.log(Kakao.isInitialized());
+
+document.getElementById("kakao-login-btn").addEventListener('click', () =>{
+    Kakao.Auth.authorize({
+        redirectUri : "https://127.0.0.1:5500/kakao-callback.html"
+    })
+})
---
+++ js/kakao-callback.js
@@ -0,0 +1,40 @@
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (code) {
+    fetch("https://kauth.kakao.com/oauth/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: "bd3c383aaed7598c42b5d40e49e54f93",
+            redirect_uri: "https://127.0.0.1:5500/kakao-callback.html",
+            code: code,
+        }),
+    })
+        .then((res) => res.json())
+        .then((tokenData) => {
+            return fetch("https://kapi.kakao.com/v2/user/me", {
+                headers: {
+                    Authorization: `Bearer ${tokenData.access_token}`,
+                },
+            });
+        })
+        .then((res) => res.json())
+        .then((userData) => {
+            const nickname = userData.kakao_account.profile.nickname;
+            const email = userData.kakao_account.email;
+
+            // 사용자 정보를 localStorage에 저장
+            localStorage.setItem("nickname", nickname);
+            localStorage.setItem("email", email);
+            localStorage.setItem("isLoggedIn", "true");
+
+            // index 페이지로 이동
+            window.location.href = "../index.html";  // 또는 index.html
+        })
+        .catch((err) => {
+            console.error("카카오 로그인 실패", err);
+            alert("로그인 실패");
+        });
+}
